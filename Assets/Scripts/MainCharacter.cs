77abf213fb5cd6d3073d50527acc1dce7f878ec0
Nguyen19092001 <nguyen19092001@gmail.com>
2025-10-10 10:57:03 +0700

    ok

---
+++ Assets/Scripts/MainCharacter.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+
+public class MainCharacter : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 5f;
+    [SerializeField]
+    private float jumpForce = 10f;
+    [SerializeField]
+    private LayerMask groundLayer;
+    [SerializeField]
+    private Transform groundCheck;
+    private Animator animator;
+    private int count = 0;
+
+    private bool isGrounded;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        count = 1;
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        rb.freezeRotation = true;
+        animator.SetBool("isSpawn", true);   
+
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        HandleMovement();
+        HandleJump();
+        UpdateAnimation();
+    }
+
+    private void HandleMovement()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+        if (moveInput > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (moveInput < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+    private void HandleJump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void UpdateAnimation()
+    {
+
+        var st = animator.GetCurrentAnimatorStateInfo(0);
+        if (st.IsName("MainSpawn"))
+        {
+            if (st.normalizedTime >= 0.99f)
+            {
+                animator.SetBool("isSpawn", false);
+                animator.SetBool("isIdle", true);
+            }
+            return; 
+        }
+        bool isRunning = Mathf.Abs(rb.linearVelocity.x) > 0.1f;
+        bool isJumping = !isGrounded;
+        bool isIdle = !isRunning && !isJumping;
+        animator.SetBool("isIdle", isIdle);
+        animator.SetBool("isRunning", isRunning);
+        animator.SetBool("isJumping", isJumping);
+    }
+}
+
