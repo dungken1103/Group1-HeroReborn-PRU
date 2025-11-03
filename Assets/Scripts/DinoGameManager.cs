c7fb1edcf026f419373ed46202b9e8a9abad3743
quocdk <doanquoc281004@gmail.com>
2025-11-03 10:04:40 +0700

    done dino game

---
+++ Assets/Scripts/DinoGameManager.cs
@@ -0,0 +1,84 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DinoGameManager : MonoBehaviour
+{
+    public static DinoGameManager Instance { get; private set; }
+    public float initialGameSpeed = 5f;
+    public float gameSpeedIncrease = 0f;
+    public float gameSpeed { get; private set; }
+
+    public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI scoreText;
+
+    public Button retryButton;
+
+    private Dinosaur player;
+    private Spawner spawner;
+
+    private float score;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            DestroyImmediate(gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance != this)
+        {
+            Instance = null;
+        }
+    }
+
+    private void Start()
+    {
+        player = FindFirstObjectByType<Dinosaur>();
+        spawner = FindFirstObjectByType<Spawner>();
+
+        NewGame();
+    }
+
+    public void NewGame()
+    {
+        Obstacle[] obstacles = FindObjectsByType<Obstacle>(FindObjectsSortMode.None);
+
+        foreach (var obstacle in obstacles)
+        {
+            Destroy(obstacle.gameObject);
+        }
+
+        gameSpeed = initialGameSpeed;
+        enabled = true;
+
+        player.gameObject.SetActive(true);
+        spawner.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(false);
+        retryButton.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        //gameSpeed += gameSpeedIncrease + Time.deltaTime;
+        score += gameSpeed * Time.deltaTime;
+        scoreText.text = Mathf.FloorToInt(score).ToString("D5");
+    }
+
+    public void GameOver()
+    {
+        gameSpeed = 0f;
+        enabled = false;
+
+        player.gameObject.SetActive(false);
+        spawner.gameObject.SetActive(false);
+        gameOverText.gameObject.SetActive(true);
+        retryButton.gameObject.SetActive(true);
+    }
+}
